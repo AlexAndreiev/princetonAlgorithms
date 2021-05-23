7e99a11d3109bd870386c3cf4b2e1980d8503495
Oleksandr Andreiev <oleksandr.andreiev@spacelabs.com>
2021-05-23 17:45:42 +0300

    Utils module has been added UnorderedMaxPQ has been added

---
+++ priorityQueue/src/com/alex/andreiev/MinPQ.java
@@ -0,0 +1,30 @@
+package com.alex.andreiev;
+
+public class MinPQ <Key extends Comparable<Key> {
+    public MinPQ() {
+
+    }
+
+    public MinPQ(Key[] arr) {
+
+    }
+
+    public void insert(Key v) {
+
+    }
+
+    public Key delMax() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public Key max() {
+
+    }
+
+    public int size() {
+    }
+}
---
+++ priorityQueue/src/com/alex/andreiev/UnorderedMaxPQ.java
@@ -0,0 +1,35 @@
+package com.alex.andreiev;
+
+public class UnorderedMaxPQ <Key extends Comparable<Key>> {
+
+    private Key[] pq; //pq[i] = ith element on pq
+    private int N;  // number of elements
+
+    public UnorderedMaxPQ(int capacity) {
+        pq = (Key[]) new Comparable[capacity]; // no generic array creation
+    }
+
+    public void insert(Key x) {
+        pq[N++] = x;
+    }
+
+    public Key delMax() {
+        int max = 0;
+        for (int i = 1; i < N; i++)
+            if (Utils.less(max, i))
+                max = i;
+        Utils.exchange(max, N-1);
+        return pq[--N];     // null out entry to prevent loitering
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+//    public Key max() {
+//
+//    }
+//
+//    public int size() {
+//    }
+}
