c1338b926e1c6c6aa7c45a965643991419137674
Oleksand Andreiev <andreyew89gmail.com>
2021-07-13 23:00:03 +0300

    minor comments changes

--- sorting/src/com/alex/andreiev/sorting/InsertionSort.java
+++ sorting/src/com/alex/andreiev/sorting/InsertionSort.java
@@ -4,11 +4,11 @@ import static com.alex.andreiev.utils.Utils.*;
 import java.util.Comparator;
 
 /*
-    To sort a randomly-order array with distinct keys, insertion sort uses ~1/4N2 compares and ~1/4N2 exchanges on average
-    Worst case performance O(n2)
-    Best case performance O(n)
-    Average case performance O(n2)
-    Worst case space complexity O(n) total, O(1) auxiliary
+    To sort a randomly-order array with distinct keys, insertion sort uses ~(1/4)*N^2 compares and ~(1/4)*N^2 exchanges on average
+    Worst case performance O(N^2)
+    Best case performance O(N^)
+    Average case performance O(N^2)
+    Worst case space complexity O(N) total, O(1) auxiliary
     Stable
 * */
 
--- sorting/src/com/alex/andreiev/sorting/SelectionSort.java
+++ sorting/src/com/alex/andreiev/sorting/SelectionSort.java
@@ -6,13 +6,13 @@ import static com.alex.andreiev.utils.Utils.*;
  Selection sort uses (N-1) + (N-2) + ... + 1 + 0 ~ (N^2)/2 compares and N exchanges
  Running time insensitive to input. Quadratic time, even if input is sorted
  Data movements is minimal. Linear number of exchanges
-    Worst case performance O(n2)
-    Best case performance O(n2)
-    Average case performance O(n2)
-    Worst case space complexity O(n) total, O(1) auxiliary
+    Worst case performance O(N^2)
+    Best case performance O(N^2)
+    Average case performance O(N^2)
+    Worst case space complexity O(N) total, O(1) auxiliary
     Not stable
 
-    Selection sort always performs O(n) swaps, while insertion sort performs O(n2) swaps in the average and worst case.
+    Selection sort always performs O(N) swaps, while insertion sort performs O(N^2) swaps in the average and worst case.
     Selection sort is preferable if writing to memory is significantly more expensive than reading.
 * */
 public class SelectionSort {
